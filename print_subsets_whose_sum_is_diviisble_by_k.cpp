c0d8d9970737433fe0fcd72d1b27cb34faef236a
gauravthedeveloper <makecocgreatagain@gmail.com>
2022-12-12 13:55:56 +0530

    new program

--- print_subsets_whose_sum_is_diviisble_by_k.cpp
+++ print_subsets_whose_sum_is_diviisble_by_k.cpp
@@ -1,8 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+void print_subsequence_divible_by_k(int index, int *array, vector<int> v, int sum, int k, int n)
+{
+    if (index == n)
+    {
+        if (sum % k == 0)
+        {
+            for (int i = 0; i < v.size(); i++)
+                cout << v[i] << " ";
+            cout << endl;
+        }
+
+        return;
+    }
+    // pick
+    sum += array[index];
+    v.push_back(array[index]);
+    print_subsequence_divible_by_k(index + 1, array, v, sum, k, n);
+    // not pick
+    v.pop_back();
+    sum -= array[index];
+    print_subsequence_divible_by_k(index + 1, array, v, sum, k, n);
+}
 int main(int argc, char const *argv[])
 {
-    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
+    vector<int> v;
+    int arr[] = {1, 2, 5};
+    int k = 3;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    print_subsequence_divible_by_k(0, arr, v, 0, k, n);
     return 0;
 }
