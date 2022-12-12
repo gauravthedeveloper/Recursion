e9b074a96676be1467160f5e523ca9c7b8c8c261
gauravthedeveloper <makecocgreatagain@gmail.com>
2022-12-13 02:14:35 +0530

    new file

---
+++ _10_matrix_recursion.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+//----------------------------------------------------------------
+// steps to reach from (0,0) to (p.first,p.second)
+void step(int x, int y, pair<int, int> p, string path)
+{
+    if (x > p.first || y > p.second)
+        return;
+    if (p.first == x && p.second == y)
+    {
+        cout << path << endl;
+        return;
+    }
+
+    if (x < p.first)
+    {
+        string line = path + "right" + " ";
+
+        step(x + 1, y, make_pair(p.first, p.second), line);
+    }
+    if (y < p.second)
+    {
+        string line = path + "up" + " ";
+        step(x, y + 1, make_pair(p.first, p.second), line);
+    }
+}
+int main(int argc, char const *argv[])
+{
+    pair<int, int> p;
+    p.first = 5;
+    p.second = 7;
+    step(0, 0, p, "");
+    return 0;
+}
